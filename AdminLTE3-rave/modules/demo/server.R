ce193772b1290be25cc1b928b2759707e0fd4f96
dipterix <dipterix.wang@gmail.com>
2022-01-24 01:27:40 -0500

    removed rave templates

--- AdminLTE3-rave/modules/demo/server.R
+++
@@ -1,7 +0,0 @@
-library(shiny)
-library(shidashi)
-
-server <- function(input, output, session, ...){
-
-  server_demo(input, output, session, ...)
-}
--- AdminLTE3-rave/modules/page500/server.R
+++
@@ -1,6 +0,0 @@
-library(shiny)
-library(shidashi)
-
-server <- function(input, output, session, ...){
-
-}
